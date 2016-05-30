a3796175d6c239f36c072989696d529860c1e488
Keefer Rourke <keeferrourke@users.noreply.github.com>
2016-05-30 10:49:19 -0400

    added license terms

--- espeak_sings.py
+++ espeak_sings.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
+# Copyright © 2015 Keefer Rourke <keefer.rourke@gmail.com>
+# <https://krourke.org>
+#
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# the COPYING file for more details.
+
 import string
 import time
 import re
